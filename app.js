a1fd3ea016c35dade457788e04c83737ad6a3c5c
madmedmod <madmedmod@gmail.com>
2019-06-19 13:03:10 +0700

    1. do it yourself form validation

---
+++ app.js
@@ -0,0 +1,36 @@
+new Vue({
+    el: '#app',
+    
+    data() {
+        return {
+            form: {
+                name: null,
+                age: null
+            }
+        }
+    },
+
+    computed: {
+        nameIsValid(){
+            return !!this.form.name
+        },
+
+        ageIsValid(){
+            return typeof this.form.age === 'number' && this.form.age > 12 && this.form.age < 120
+        },
+
+        formIsValid(){
+            return this.nameIsValid && this.ageIsValid
+        }
+    },
+
+    methods: {
+        submitForm(){
+            if (this.formIsValid) {
+                console.log('📝 Form Submitted', this.form)
+            } else {
+                console.log('❌ Invalid form')
+            }
+        }
+    }
+})
